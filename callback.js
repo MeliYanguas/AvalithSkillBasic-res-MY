8f9f73b2b09a4112af9953284f3229863cb65880
Rocio Yang <melinayanguas@gmail.com>
2021-07-27 20:14:21 -0300

    some corrections

--- callback.js
+++ callback.js
@@ -1,24 +1,21 @@
 /*
     Completar el siguiente segmento de codigo utilizando el concepto de callback para
     que los mensajes se muestren de manera correcta (1,2,3,4)
-
     Solo puedes modificar o eliminar los comentarios marcados con "TODO" como crea necesario.
 */
 
 console.log(1);
 
-function esperar2segundos(/* TODO */) {
+function esperar2segundos(cb) {
     console.log(2)
-    setTimeout((/* TODO */) => {
+    setTimeout(() => {
         console.log(3)
-        alTerminar()
+       cb()
     }, 2000);
-    /* TODO */
 }
 
 function alTerminar() {
-    /* TODO */
     console.log(4)
 }
 
-esperar2segundos(/* TODO */);
+esperar2segundos(alTerminar);
